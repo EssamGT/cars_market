2944c5eadf6597a3663659179f9cfd31936425d3
Essam Ghuraib <Essamgh94@gmail.com>
2026-02-23 23:47:05 +0200

    sell cubit _update
    Signed-off-by: Essam Ghuraib <Essamgh94@gmail.com>

--- packages/features/home/sell/lib/presentation/cubit/sell_cubit.dart
+++ packages/features/home/sell/lib/presentation/cubit/sell_cubit.dart
@@ -80,7 +80,6 @@ class SellCubit extends Cubit<SellState> {
         car.carCondition = condition;
       }
     }
-    print(car.carCondition.getConditionText());
   }
 
   void setTransmissionType(TransmissionType transmissionType) {
